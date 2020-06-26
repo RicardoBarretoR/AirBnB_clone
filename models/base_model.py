500ba9efe0dd6ea3e46ac523c0e3936d66dd7a9a
CrisRuedaP <crisruedap@gmail.com>
2020-06-26 18:51:50 +0000

    up models directory

---
+++ models/base_model.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""a class that defines all common attributes/methods for other classes"""
+from datetime import datetime
+import models
+import uuid
+
+
+class BaseModel:
+    """class base"""
+    def __init__(self, *args, **kwargs):
+        """"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == created_at and key == updated_at:
+                    value = datetime.strptime(created_at, updated_at)
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return a srting representation"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """updates the public instance attribute"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values"""
+        newdict = dict(self.__dict__)
+        newdict["__class__"] = self.__class__.__name__
+        newdict["created_at"] = datetime.isoformat(newdict["created_at"])
+        newdict["updated_at"] = datetime.isoformat(newdict["updated_at"])
+        return newdict
