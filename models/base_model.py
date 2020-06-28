0cd86ff983404fed5e3091b3560be6a89fb844f6
CrisRuedaP <crisruedap@gmail.com>
2020-06-28 21:03:38 +0000

    adding variable storage

--- models/base_model.py
+++ models/base_model.py
@@ -11,14 +11,15 @@ class BaseModel:
         """"""
         if kwargs:
             for key, value in kwargs.items():
-                if key == created_at and key == updated_at:
-                    value = datetime.strptime(created_at, updated_at)
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """Return a srting representation"""
@@ -28,6 +29,7 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values"""
